4f867eb957de4811d542d817ea4436f5b6fe81a2
xuetaoli <15361286+fantancy196@user.noreply.gitee.com>
2025-01-14 22:19:27 +0800

    自动提交

--- auto-push.sh
+++ auto-push.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
-git add .
-git commit -m"自动提交"
-git push origin master
+#!/bin/bash
+
+# 检查是否有未提交的更改
+if [[ -n $(git status -s) ]]; then
+  # 如果有未提交的更改，执行提交和推送
+  echo "检测到未提交的更改，正在提交并推送..."
+  git add .
+  git commit -m "自动提交"
+  git push origin master
+else
+  # 如果没有未提交的更改，输出提示
+  echo "没有未提交的更改。"
+fi
